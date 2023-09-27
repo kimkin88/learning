fb0f8c3644852563904ec2f1584aa90c499b9dd2
kimkin88 <kimuandr@gmail.com>
2023-09-27 10:56:02 +0300

    sidebar, navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,19 @@
-import { Suspense } from "react";
-import { Link, Route, Routes } from "react-router-dom";
 import "./styles/index.scss";
 import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme();
+  const { theme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
-      <button onClick={toggleTheme}>Toggle Theme</button>
-      <Link to={"/"}>Главная</Link>
-      <Link to={"/about"}>О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={"/about"} element={<AboutPage />} />
-          <Route path={"/"} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <Navbar />
+      <div className="content-page">
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   );
 };
---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,25 @@
+import { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { routeConfig } from "shared/config/routeConfig/routeConfig";
+
+const AppRouter = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        {Object.values(routeConfig).map(({ path, element }) => (
+          <Route
+            key={path}
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <div className="page-wrapper">{element}</div>
+              </Suspense>
+            }
+            path={path}
+          />
+        ))}
+      </Routes>
+    </Suspense>
+  );
+};
+
+export default AppRouter;
