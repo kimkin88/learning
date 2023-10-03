30c984e6cf045a5e48fb84205b4a96b77e89b747
kimkin88 <kimuandr@gmail.com>
2023-10-03 13:39:08 +0300

    loader, error page

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,24 +1,23 @@
 import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { routeConfig } from "shared/config/routeConfig/routeConfig";
+import { PageLoader } from "shared/ui/PageLoader/PageLoader";
 
 const AppRouter = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        {Object.values(routeConfig).map(({ path, element }) => (
-          <Route
-            key={path}
-            element={
-              <Suspense fallback={<div>Loading...</div>}>
-                <div className="page-wrapper">{element}</div>
-              </Suspense>
-            }
-            path={path}
-          />
-        ))}
-      </Routes>
-    </Suspense>
+    <Routes>
+      {Object.values(routeConfig).map(({ path, element }) => (
+        <Route
+          key={path}
+          element={
+            <Suspense fallback={<PageLoader />}>
+              <div className="page-wrapper">{element}</div>
+            </Suspense>
+          }
+          path={path}
+        />
+      ))}
+    </Routes>
   );
 };
 
